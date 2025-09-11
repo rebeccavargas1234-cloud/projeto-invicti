ad17d6da427e0e6805b23737ab6cbd8eb86b6520
Rebecca Cristina Vargas do Carmo <rebecca.vargas@software.com.br>
2025-09-11 15:09:37 -0300

    Adicionando pagina labrador

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def border():
     return render_template("border.html")
 
+@app.route("/labrador")
+def labrador():
+    return render_template("labrador.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
